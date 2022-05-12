6736322aa67d647cbfbce3d043ded708cb81831b
prantik.sarmah <prantik.sarmah@grapecity.com>
2022-05-12 13:44:00 +0530

    Adding complete

---
+++ C1DataQueryApp/C1DataQueryApp/Pages/DataQueryGenerator/JsonData.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace C1DataQueryApp.Pages.DataQueryGenerator
+{
+    public class JsonDataModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ C1DataQueryApp/C1DataQueryApp/wwwroot/js/jsonData.js
@@ -0,0 +1,19 @@
+﻿function JsonFileChange(th) {
+    $("#upload-file-info").html($(th).val());
+    import("https://localhost:7035/js/jsonView/view.js")
+        .then(function (jsonView) {
+            let file = th.files[0];
+            let reader = new FileReader();
+            reader.readAsText(file);
+            reader.onload = function () {
+                var tree = jsonView.create(reader.result);
+                jsonView.render(tree, document.querySelector('.jsontree'));
+
+            };
+
+            reader.onerror = function () {
+            };
+        }, function (err) {
+            alert(err);
+        });
+}
